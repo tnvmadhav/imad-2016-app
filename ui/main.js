cf3b0b5d8b6e930251cd2a3f25371ef0b69c5238
tnvmadhav <tnvmadhav@gmail.com>
2016-11-17 23:32:23 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,8 +1,4 @@
 console.log('Loaded!');
 
-
-//image animation
-var img = document.getElementById('madi');
-img.onclick =function () {
-    img.style.marginLeft ='100px';
-    };
+var v= document.getElementById('kkk');
+v.innerHTML = Yo;
