8e21617ec27af4ef27cf90aa833d325a4ec050ce
tnvmadhav <tnvmadhav@gmail.com>
2016-11-17 23:02:02 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -12,6 +12,9 @@ app.get('/', function (req, res) {
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
+app.get('/ui/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+});
 
 var articles = {
 'article-one': {
