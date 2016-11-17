91891a20cbcb7ee4c41800e4926ad36bb351da32
tnvmadhav <tnvmadhav@gmail.com>
2016-11-18 00:01:58 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,6 +3,13 @@ console.log('Loaded!');
 var img = document.getElementById('madi');
 
 img.onclick = function ()
+{    
+    var marginLeft = 0;
+    function MoveRight()
 {
+   marginLeft = marginLeft +5;
+   img.style.marginLeft = marginLeft+"px";
+}
+     var interval = setInterval(MoveRight, 50);
     img.style.marginLeft = "100px";
 };
