5b55ffc274bcc96e9dd7adf6134e61d3c0337cb3
tnvmadhav <tnvmadhav@gmail.com>
2016-11-18 17:55:35 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,16 +1,2 @@
 console.log('Loaded!');
 
-var img = document.getElementById('madi');
-
-var height = 200;
- function expand() {
-   
-   height = height - 0.33;
-   img.style.height = height+ 'px';
-}
-img.onclick = function ()
-{    
-    print("Hey you just clicked on me");
-    print("Better watch your finger");
-   var interval = setInterval(expand, 40);
-};
