142b25c76f98af2154a67f6cc94052d03ad8c8d7
tnvmadhav <tnvmadhav@gmail.com>
2016-11-18 17:46:50 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,9 +5,8 @@ var img = document.getElementById('madi');
 var height = '150px';
  function expand() {
    
-   height = height + 100;
-   
-   img.style.height = height+"px";
+   height = height + 50;
+   img.style.height = height+ 'px';
 }
 img.onclick = function ()
 {    
