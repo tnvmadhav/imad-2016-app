065e19a84007b5c23f6beafa1b5c113ed521edf1
T .VenuMadhav <tnvmadhav@gmail.com>
2016-11-19 22:59:34 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -63,3 +63,42 @@ var name = inputName.value;
   request.send(null);
   
 }; 
+
+
+
+
+
+//comment box
+var submit2 = document.getElementById('submit_btn2');
+submit2.onclick  = function() {
+  //Send a request to the server and send the names
+  //Capture the list of names and render it in the list
+ 
+    
+   //Create a request object 
+  var request =  new XMLHttpRequest();  
+  
+  //Capture a response and store it in a variable
+  request.onreadystatechange = function() {
+      if(request.readyState === XMLHttpRequest.DONE){
+          //take some action 
+          if(request.status ===200) {
+             var names = request.responseText;
+             names = JSON.parse(names);
+          var  list = '';
+          for(var i=0; i<names.length; i++){
+     list += '<li>'+ names[i] + '</li>';
+     }
+ var ul  = document.getElementById('namelist');
+ ul.innerHTML = list;
+          }
+      }
+  };
+  
+  //Make request 
+  var inputName = document.getElementById('name');
+var name = inputName.value;
+  request.open('GET','http://tnvmadhav.imad.hasura-app.io/submit-name?name='+name,true);
+  request.send(null);
+  
+}; 
