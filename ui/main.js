4a4e67946e9dc1f6318f05bb677eacae8287249b
T .VenuMadhav <tnvmadhav@gmail.com>
2016-11-19 23:01:38 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -89,7 +89,7 @@ submit2.onclick  = function() {
           for(var i=0; i<names.length; i++){
      list += '<li>'+ names[i] + '</li>';
      }
- var ul  = document.getElementById('namelist');
+ var ul  = document.getElementById('namelist2');
  ul.innerHTML = list;
           }
       }
