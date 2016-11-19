dba4c53db5b571489d09cce226b3fe4dfde2665d
T .VenuMadhav <tnvmadhav@gmail.com>
2016-11-19 22:11:04 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -46,11 +46,11 @@ submit.onclick  = function() {
           if(request.status ===200) {
              var names = request.responseText;
              names = JSON.parse(names);
- var  list = '';
- for(var i = 0; i< names.length; i++){
+          var  list = '';
+          for(var i=0; i<names.length; i++){
      list += '<li>'+ names[i] + '</li>';
- }
- var ul  = document.getElementByID('namelist');
+     }
+ var ul  = document.getElementById('namelist');
  ul.innerHTML = list;
           }
       }
