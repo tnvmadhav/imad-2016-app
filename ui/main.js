df9d6f3a3285a08c30cb1d67b16972a25a407718
T .VenuMadhav <tnvmadhav@gmail.com>
2016-11-19 18:34:24 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,2 +1,27 @@
-console.log('Loaded!');
+//counter  code
+var button = document.getElementById('counter');
 
+
+
+button.onclick = function() {
+    
+   //Create a request object 
+  var request =  new XMLHttpRequest();  
+  
+  //Capture a response and store it in a variable
+  request.onreadystatechange = function() {
+      if(request.readyState === XMLHttpRequest.DONE){
+          //take some action 
+          if(request.status ===200) {
+              var counter = request.responseText;
+              var span = document.getElementById('count');
+              span.innerHTML = counter.toString();
+          }
+      }
+  };
+  
+  //Make request 
+  request.open('GET','http://tnvmadhav.imad.hasura-app.io/counter',true);
+  request.send(null);
+    
+};
