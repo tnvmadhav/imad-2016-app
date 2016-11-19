0da72224b0d24461200fea319b2ccbf812be7015
T .VenuMadhav <tnvmadhav@gmail.com>
2016-11-19 21:00:55 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -117,7 +117,7 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
-var names[];
+var names = [];
 app.get('/submit-name/:name', function(req,res) {
     //Get name from request
     var name = req.params.name;
