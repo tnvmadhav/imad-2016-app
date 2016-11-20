96b6df95bcb65532b7a3d0af1d237833493b6637
T .VenuMadhav <tnvmadhav@gmail.com>
2016-11-20 19:00:14 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -144,10 +144,21 @@ app.get('/submit-comment', function(req,res) { //submit-name?name=xxxxxx
      res.send(JSON.stringify(namesc));
 });
 
-app.get('/:ArticleName', function(req,res) {
-   var ArticleName =  req.params.ArticleName;
-    res.send(createTemplate(articles[ArticleName]));
-});
+app.get('/articles/:ArticleName', function(req,res) {
+   
+   pool.query("SELECT * FROM article WHERE title = '"+req.params.ArticleName + "'", function(err,result){
+       if(err){
+           res.status(500).send(err.toString());
+       } else {
+           if(result.rows.length === 0) {
+               res.status(404).send('Article not found');
+           } else {
+               articleData = result.rows[0];
+                res.send(createTemplate(articleData));
+           }
+       }
+      });
+   });
 
 app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
